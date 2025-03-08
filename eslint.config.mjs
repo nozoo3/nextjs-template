e3ea198be628639681d729a1a43de533cc41a8da
nozomi <dog1518rocky@gmail.com>
2025-03-08 17:26:31 +0900

    Add configuration files and update project settings for Prettier, ESLint, and Tailwind CSS

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,37 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintConfigPrettier from 'eslint-config-prettier';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:tailwindcss/recommended',
+    'prettier',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  ignores: [
+    'node_modules/',
+    '.next/',
+    'public/',
+    'package-lock.json',
+    'next.config.js',
+    'tsconfig.json',
+    'src/next-env.d.ts',
+    '*.cjs',
+    '*.mjs',
+  ],
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  eslintConfigPrettier,
 ];
 
 export default eslintConfig;
