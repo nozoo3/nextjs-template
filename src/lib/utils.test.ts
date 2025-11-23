b2cb602c251bfee8a24c09d35b054857e94d24f6
nozomi <dog1518rocky@gmail.com>
2025-11-23 15:58:16 +0900

    feat: tailwind utils

---
+++ src/lib/utils.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, it } from 'vitest';
+
+import { cn } from './utils';
+
+describe('cn', () => {
+  it('should merge class names', () => {
+    expect(cn('c1', 'c2')).toBe('c1 c2');
+  });
+
+  it('should handle conditional classes', () => {
+    expect(cn('c1', true && 'c2', false && 'c3')).toBe('c1 c2');
+  });
+
+  it('should merge tailwind classes', () => {
+    expect(cn('p-4 p-2')).toBe('p-2');
+  });
+});
